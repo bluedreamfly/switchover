fa863f9779745afe5f2b9319fe22568e1a20d2b1
hzhwalkbydream <hzhwalkbydream@gmail.com>
2017-07-18 15:21:56 +0800

    初始化项目

---
+++ Switchover/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Switchover
+//
+//  Created by zhenhui huang on 2017/7/18.
+//  Copyright © 2017年 walkbydream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Switchover/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Switchover
+//
+//  Created by zhenhui huang on 2017/7/18.
+//  Copyright © 2017年 walkbydream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
